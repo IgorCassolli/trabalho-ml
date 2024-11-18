b74c59378735b30befd5dbf3849ba5ea89a368f0
Igor Cassolli <igorcassolli1305@gmail.com>
2024-11-18 20:08:42 -0300

    Ultimos ajustes e pptx

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import altair as alt
 import util
 import data_handler
 import pickle
@@ -13,46 +14,71 @@ from sklearn.preprocessing import OrdinalEncoder
 # para rodar esse arquivo
 # streamlit run app.py
 
-print("Abriu a pagina")
+st.set_page_config(layout="wide")
 
-# verifica se a senha de acesso está correta
-#if not util.check_password():
-#    # se a senha estiver errada, para o processamento do app
-#    print("Usuario nao logado")
-#    st.stop()
+st.title("Trabalho do Grau B - Ciências de Dados e Big Data")
+st.header("Autores: ")
+st.subheader("Aluno 1: Jones Marlos Pinheiro da Rosa")
+st.subheader("Aluno 2: Igor da Silva Cassolli", divider="gray")
 
-print("Carregou a pagina")
+st.markdown("<a href='https://www.kaggle.com/datasets/rabieelkharoua/predict-online-gaming-behavior-dataset'>"+
+            "Dataframe: Predict Online Gaming Behavior Dataset</a>", unsafe_allow_html=True)
 
-# Aqui começa a estrutura do App que vai ser executado em produção (nuvem AWS)
+st.markdown("<h5 style='text-align: left;'>Modelagem preditiva com <span style='color: red;'>objetivo"+
+            "</span> de predizer <span style='color: red;'>a retenção de um jogador.</span></h5>", unsafe_allow_html=True)
 
-# primeiro de tudo, carrega os dados para um dataframe
+st.header('Dados do Dataset')
+
+#Carregar CSV
 dados = data_handler.load_data()
 
-# carrega o modelo de predição já treinado e validado
+#Carregar modelo treinado
 model = pickle.load(open('./models/final_classification_model_games.pkl', 'rb'))   
 
-# começa a estrutura da interface do sistema
-st.title('Predict Online Gaming Behavior')
+on = st.toggle('Exibir análise dos dataset')
 
-data_analyses_on = st.toggle('Show dataset')
-
-if data_analyses_on:
-    # essa parte é só um exemplo de que é possível realizar diversas visualizações e plotagens com o streamlit
-    st.header('Gaming Behavior - Dataframe')
-    
-    # exibe todo o dataframe dos dados
+if on:
     st.dataframe(dados)
 
-    # plota um gráfico de barras com a contagem dos dados
-    st.header('EngagementLevel')
+    st.subheader('Contagem por Engajamento')
     st.bar_chart(dados.EngagementLevel.value_counts())
+
+    st.subheader('Contagem por gênero')
+    st.bar_chart(dados.Gender.value_counts())
+
+    st.subheader("Horas Jogadas por Gênero e Tipo de Jogo")
+    avg_playtime_by_genre_gender = dados.groupby(["GameGenre", "Gender"])["PlayTimeHours"].mean().reset_index()
+
+    chart_data = avg_playtime_by_genre_gender.pivot(index="GameGenre", columns="Gender", values="PlayTimeHours")
+
+    fig, ax = plt.subplots(figsize=(16, 6))
+    for gender in chart_data.columns:
+        ax.plot(chart_data.index, chart_data[gender], label=gender, marker='o')
+    ax.set_xlabel("Gênero do Jogo", fontsize=10)
+    ax.set_ylabel("Horas Jogadas (Média)", fontsize=10)
+    ax.legend(title="Gênero", fontsize=10)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    st.subheader("Nível Médio dos Jogadores por Engajamento")
+    avg_level_by_engagement = dados.groupby("EngagementLevel")["PlayerLevel"].mean()
+    st.bar_chart(avg_level_by_engagement)
+
+    st.subheader("Distribuição de Jogadores por Dificuldade do Jogo")
+    difficulty_counts = dados['GameDifficulty'].value_counts()
+    st.bar_chart(difficulty_counts)
     
-# daqui em diante vamos montar a inteface para capturar os dados de input do usuário para realizar a predição
-st.header('Engagement Level Predict')
+st.header('Preditor de retenção do jogador')
+
+inGamePurchasesMap = {
+    "Não": 0,
+    "Sim": 1,
+}
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    age = st.number_input('Age in years', step=1, value=43)
+    age = st.number_input('Age in years', step=1, value=0)
 with col2:
     gender = st.selectbox("Gender", ("Male", "Female"))
 with col3:
@@ -64,23 +90,22 @@ with col1:
 with col2:
     game_difficulty = st.selectbox("Game Difficulty", ("Easy", "Medium", "Hard"))
 with col3:
-    in_game_purchases = st.number_input('In Game Purchases', step=1, value=0)
+    in_game_purchases = inGamePurchasesMap[st.selectbox("In Game Purchases", ("Não", "Sim"))]
 with col4:
-    achievements_unlocked = st.number_input('Achievements Unlocked', step=1, value=25)
+    achievements_unlocked = st.number_input('Achievements Unlocked', step=1, value=0)
 
 col1, col2, col3, col4 = st.columns(4)
 with col1:
-    play_time_hours = st.number_input('Play Time Hours', step=0.1, value=16.3)
+    play_time_hours = st.number_input('Play Time Hours', step=0.1, value=0.0)
 with col2:
-    sessions_per_week = st.number_input('Sessions Per Week', step=1, value=6)
+    sessions_per_week = st.number_input('Sessions Per Week', step=1, value=0)
 with col3:
-    avg_session_duration_minutes = st.number_input('Avg Session in Minutes', step=1, value=108)
+    avg_session_duration_minutes = st.number_input('Avg Session in Minutes', step=1, value=0)
 with col4:
-    player_level = st.number_input('Player Level', step=1, value=79)
+    player_level = st.number_input('Player Level', step=1, value=0)
 
-submit = st.button('Predict Engagement')
+submit = st.button('Predizer')
 
-# data mapping
 standard_scaler_age = pickle.load(open('./models/standard_scaler_age.pkl', 'rb'))
 one_hot_encoder_gender = pickle.load(open('./models/one_hot_encoder_gender.pkl', 'rb'))
 one_hot_encoder_location = pickle.load(open('./models/one_hot_encoder_location.pkl', 'rb'))
@@ -92,8 +117,7 @@ standard_scaler_sessions_per_week = pickle.load(open('./models/standard_scaler_s
 standard_scaler_avg_session_duration_minutes = pickle.load(open('./models/standard_scaler_avg_session_duration_minutes.pkl', 'rb'))
 standard_scaler_player_level = pickle.load(open('./models/standard_scaler_player_level.pkl', 'rb'))
 standard_scaler_achievements_unlocked = pickle.load(open('./models/standard_scaler_achievements_unlocked.pkl', 'rb'))
- 
-# verifica se o botão submit foi pressionado
+
 if submit:
     x_age = standard_scaler_age.transform(np.array(age).reshape(1, -1))
     x_gender = one_hot_encoder_gender.transform(np.array(gender).reshape(1, -1))
@@ -107,22 +131,18 @@ if submit:
     x_player_level = standard_scaler_player_level.transform(np.array(player_level).reshape(1, -1))
     x_achievements_unlocked = standard_scaler_achievements_unlocked.transform(np.array(achievements_unlocked).reshape(1, -1))
     
-    x_data = np.concatenate((x_age, x_gender, x_location, x_game_genre, x_play_time_hours, x_in_game_purchases, x_game_difficulty, x_sessions_per_week, x_avg_session_duration_minutes, x_player_level, x_achievements_unlocked), axis=1)
-    
-    print(x_data.shape)
-    print(x_data)
+    x_data = np.concatenate((x_age, x_gender, x_location, x_game_genre, 
+                             x_play_time_hours, x_in_game_purchases, x_game_difficulty, 
+                             x_sessions_per_week, x_avg_session_duration_minutes, x_player_level,
+                             x_achievements_unlocked), axis=1)
     
     # Realiza a predição
     results = model.predict(x_data)
-    print(results)
 
-    # Exibe a classe predita (nivel de engajamento)
     if len(results) == 1:
         st.subheader(results[0])
     
-    # Realiza a predição com a probabilidade de cada classe
     results_proba = model.predict_proba(x_data)
-    print(results_proba)
 
     # Exibe a probabilidade de cada classe    
     if len(results_proba) == 1:
